e8deca171900c561f0a5681d3140a41922a9e8e5
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2024-07-07 16:20:39 +0300

    Make class Ship

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunken = false;
+  }
+
+  hit = () => {
+    this.hits++;
+  };
+
+  isSunk = () => {
+    return this.hits == this.length ? true : false;
+  };
+}
