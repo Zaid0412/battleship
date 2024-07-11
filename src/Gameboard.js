ccf05e4857189a9aea868d515da559ce16eab4d6
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2024-07-11 12:41:59 +0300

    Make resetBoard() and displayShips()

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,11 +1,14 @@
 import { Ship } from "./Ship";
 
+const playerSquares = document.querySelectorAll(".player-board .board-square");
+
 export class Gameboard {
   constructor() {
     this.fleet = [
       new Ship(5),
       new Ship(4),
       new Ship(4),
+      new Ship(3),
       new Ship(3),
       new Ship(3),
       new Ship(2),
@@ -27,7 +30,8 @@ export class Gameboard {
     this.missedHits = [];
   }
 
-  initGame = () => {
+  initGame = (comp) => {
+    this.resetBoard();
     for (const ship of this.fleet) {
       // Generating random x and y coords
       const randomXCoord = Math.trunc(Math.random() * 10);
@@ -50,19 +54,63 @@ export class Gameboard {
       }
     }
     console.log(this.board);
+    if (comp == "comp") {
+      return;
+    } else {
+      this.displayShips();
+    }
   };
 
-  allShipSink = () => {
+  resetBoard = () => {
+    for (const square of playerSquares) {
+      square.style.backgroundColor = "rgb(174, 202, 228, 0.445)";
+    }
+    this.board = [
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+      ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ];
+  };
+
+  isFleetSunk = () => {
     // Checking is all of the ships are sunken
-    return this.fleet.every((ship) => ship.isSunk());
+    return this.fleet.every((ship) => ship.sunken);
   };
 
   receiveAttack = (x, y) => {
     // Checking if it is a hit
     if (this.board[y][x] !== "-") {
       this.board[y][x].hit(); // If yes, call ship.hit() to that ship
+      return true;
     } else if (this.board[y][x] === "-") {
       this.missedHits.push([x, y]); // if not, adding the missed coords to missedHits array
+      return false;
+    }
+  };
+
+  displayShips = () => {
+    for (let [y, arrLine] of this.board.entries()) {
+      // Looping over this.board
+      for (let [x, arrSquare] of arrLine.entries()) {
+        if (arrSquare !== "-") {
+          // If the current elemnet isn't "-"
+          let xCoord = x + 1;
+          let yCoord = y + 1;
+
+          for (const square of playerSquares) {
+            if (square.dataset.x == xCoord && square.dataset.y == yCoord) {
+              square.style.backgroundColor = "green";
+            }
+          }
+        }
+      }
     }
   };
 }
